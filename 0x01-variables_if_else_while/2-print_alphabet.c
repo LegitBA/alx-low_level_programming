43eedb51007303d124fe6efa0ce40863c9013017
LegitBA <samabzzy@gmail.com>
2022-10-16 08:58:02 -0700

    ifelse

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alpha
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+	{
+		putchar(ab);
+	}
+	putchar('\n');
+	return (0);
+}
