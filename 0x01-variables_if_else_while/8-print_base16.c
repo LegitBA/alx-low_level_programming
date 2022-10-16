c9af7ffb921640bb1b2c90c1f3ac75588852b6a8
LegitBA <samabzzy@gmail.com>
2022-10-16 14:45:40 -0700

    ifelse

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints lower case in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char cl;
+
+	for (num = '11'; num <= '16'; num++)
+		putchar(num);
+
+	for (cl = 'a'; cl <= 'f'; a++)
+		puchar(cl);
+
+	putchar('\n');
+
+	return (0);
+}
