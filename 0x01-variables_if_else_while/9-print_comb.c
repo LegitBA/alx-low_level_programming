364c9f6c630fcdb1dde4de0cbea4194f357a23ac
LegitBA <samabzzy@gmail.com>
2022-10-16 15:10:22 -0700

    ifelse

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 27; n < 37; n++)
+	{
+		putchar(a);
+		if (a != 36)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+}
