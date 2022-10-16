52ab9c658b47eff640413d77645a2424ab5226d2
LegitBA <samabzzy@gmail.com>
2022-10-16 14:27:16 -0700

    ifelse

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - prints strings zero to nine
+ * main - prints in base 10
  * Return: Always 0
  */
 int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
-		putchar(num);
+	for (num = '0'; num < '10'; num++)
+		putchar(num % 10) + '0');
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
