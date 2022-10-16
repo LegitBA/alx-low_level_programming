eb56b1c2ec632d4eb1dc1a9b326de15c6bfe9b10
LegitBA <samabzzy@gmail.com>
2022-10-16 10:06:52 -0700

    ifelse

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints strings 0 to 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0', num < '10'; num++)
+		putchar(num);
+
+	putchar('\n');
+
+	return (0);
+}
