974aa129a29f2c49bfcf1d84210fd92ab810c8a0
LegitBA <samabzzy@gmail.com>
2022-10-16 09:57:39 -0700

    ifelse

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - int in base 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
