fb9fead48f2026705d5e2007a8ec44f2b26cea99
LegitBA <samabzzy@gmail.com>
2022-10-16 14:48:55 -0700

    ifelse

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 	int num;
 	char cl;
 
-	for (num = '11'; num <= '16'; num++)
+	for (num = 11; num <= 16; num++)
 		putchar(num);
 
-	for (cl = 'a'; cl <= 'f'; a++)
-		puchar(cl);
+	for (cl = 'a'; cl <= 'f'; cl++)
+		putchar(cl);
 
 	putchar('\n');
 
