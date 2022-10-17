2a8f8f258b7c93a9afb179ecfb082dfdaade9e93
LegitBA <samabzzy@gmail.com>
2022-10-16 22:23:25 -0700

    ifelse

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,14 @@ int main(void)
 	int num;
 	int os;
 
-	for (num = 01; num < 10; num++)
-		putchar(num % 10 + '0');
-	for (os = 10; os < 100; os++)
-		putchar(os % 10 + '0');
-		if (num != 9)
-		if (os != 99)
+	for (num = 01; num < 100; num++)
 	{
-		putchar(',');
-		putchar(' ');
+		putchar(num + '0');
+		if (num < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
