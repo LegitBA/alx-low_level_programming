8415949674cf72d1439733516ba46175736134fa
LegitBA <samabzzy@gmail.com>
2022-10-18 00:20:20 -0700

    ifelse

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 
 	while (num1 <= 9)
 	{
-		num3 = 0;
+		num2 = 0;
 		while (num2 <= 9)
 		{
 			num3 = 0;
-			while (num <= 9)
+			while (num3 <= 9)
 			{
 				if (num1 != num2 && num1 < num2 && num2 != num3 && num2 < num3)
 				{
