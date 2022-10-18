4f4553c71b981c7eabf5792ba77045eadd978acc
LegitBA <samabzzy@gmail.com>
2022-10-17 23:26:58 -0700

    ifelse

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,16 @@ int main(void)
 {
 	int num1 = 0, num2;
 
-	while (num2 <= 9)
+	while (num2 <= 99)
 	{
 	num2 = 0;
-		while (num2 <= 9)
+		while (num2 <= 99)
 		{
 			if (num1 != num2 && num1 < num2)
 			{
 				putchar(num1 + 48);
 				putchar(num2 + 48);
-				
+
 				if (num1 + num2 != 17)
 				{
 					putchar(',');
