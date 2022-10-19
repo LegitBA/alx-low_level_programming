189ab9dcd90e741c7b73c05d03ae0494b6711d9d
LegitBA <samabzzy@gmail.com>
2022-10-19 08:02:15 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#iclude "main.h"
+
+/**
+ * main - prints alphabet in lower case
+ * Return: Always 0
+ */
+inint main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
