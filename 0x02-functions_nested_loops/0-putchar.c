8d6105ee259439f8a8383e2e485173e1ab4892e6
LegitBA <samabzzy@gmail.com>
2022-10-19 03:55:25 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - prints zero putchar
+ * Return: always 0
+ */
+int main(void)
+{
+	int i
+
+		for (i = 0; i <= 1; i++)
+			putchar(_putchar);
+	putchar('\n');
+	return (0);
+}
