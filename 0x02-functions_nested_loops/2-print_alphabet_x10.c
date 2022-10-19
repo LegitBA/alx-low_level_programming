5e315f0e11f89cdcb75175d32487d309e57cfc92
LegitBA <samabzzy@gmail.com>
2022-10-19 08:47:51 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int lb;
+	
+	for (lb = 0; lb <= 9; lb++)
+	{
+		char la;
+
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
