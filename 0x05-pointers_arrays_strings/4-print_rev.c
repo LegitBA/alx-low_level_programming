04951e68fc451459af529b9520784d6f8526533e
LegitBA <samabzzy@gmail.com>
2022-10-24 15:18:28 -0700

    array

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reverse
+ * @s: string
+ * Return: length of reversed string
+ */
+void print_rev(char *s)
+{
+	int = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i =i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
