77ea2144d8ca3d341759a603aa326778294f83eb
LegitBA <samabzzy@gmail.com>
2022-11-08 03:21:58 -0800

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the prog
+ * @qrgc: count arguement
+ * @argv: argument
+ * Return:Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /*ignore argv*/
+	printf("%:\n", argc - 1);
+
+	return (0);
+}
