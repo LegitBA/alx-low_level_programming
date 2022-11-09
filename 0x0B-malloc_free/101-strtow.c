c72a9922ff01bb3aa0b797b4b5bbf77c2081fc53
LegitBA <samabzzy@gmail.com>
2022-11-08 21:43:10 -0800

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -54,7 +54,7 @@ char **strtow(char *str)
 	char **strings;
 	int index = 0, words, w, letters, l;
 
-	if (st == NULL || str[0] == '\0')
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
 	words = count_words(str);
