ab8d8c75870e4f9b4f3d91fd640434308ee8a331
LegitBA <samabzzy@gmail.com>
2022-11-28 04:12:08 -0800

    lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void _attribute_((constructor)) hare(void);
 
 void hare(void)
 {
-	pintf("You're beat! and yet, you must allow,
-		\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+       printf("I bore my house upon my back!\n");
 }
